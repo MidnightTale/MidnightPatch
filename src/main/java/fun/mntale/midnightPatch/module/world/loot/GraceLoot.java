6b17d81c16104b23872b90680fe932f041ab3bf0
MidnightTale <midnighttale24@gmail.com>
2025-06-23 03:13:48 +0700

    change armor loot to book

--- src/main/java/fun/mntale/midnightPatch/module/world/loot/GraceLoot.java
+++ src/main/java/fun/mntale/midnightPatch/module/world/loot/GraceLoot.java
@@ -6,17 +6,13 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class GraceLoot extends AbstractLootHandler {
     private static final NamespacedKey GRACE_KEY = NamespacedKey.fromString("midnightpatch:grace");
     private static final Enchantment GRACE_ENCHANT = io.papermc.paper.registry.RegistryAccess.registryAccess()
         .getRegistry(io.papermc.paper.registry.RegistryKey.ENCHANTMENT)
         .get(GRACE_KEY);
-    private static final Material[] ARMOR_TYPES = {
-        Material.DIAMOND_HELMET, Material.DIAMOND_CHESTPLATE, Material.DIAMOND_LEGGINGS, Material.DIAMOND_BOOTS,
-        Material.IRON_HELMET, Material.IRON_CHESTPLATE, Material.IRON_LEGGINGS, Material.IRON_BOOTS
-    };
+
     private static final String[] LOOT_TABLES = {
         "minecraft:chests/end_city_treasure"
     };
@@ -28,10 +24,9 @@ public class GraceLoot extends AbstractLootHandler {
 
     @Override
     protected ItemStack createLootItem() {
-        Material armorType = ARMOR_TYPES[ThreadLocalRandom.current().nextInt(ARMOR_TYPES.length)];
-        ItemStack armor = new ItemStack(armorType);
-        armor.addUnsafeEnchantment(GRACE_ENCHANT, 1);
-        return armor;
+        ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
+        book.addUnsafeEnchantment(GRACE_ENCHANT, 1);
+        return book;
     }
 
     @Override
--- src/main/java/fun/mntale/midnightPatch/module/world/loot/ResilienceLoot.java
+++ src/main/java/fun/mntale/midnightPatch/module/world/loot/ResilienceLoot.java
@@ -6,17 +6,13 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class ResilienceLoot extends AbstractLootHandler {
     private static final NamespacedKey RESILIENCE_KEY = NamespacedKey.fromString("midnightpatch:resilience");
     private static final Enchantment RESILIENCE_ENCHANT = io.papermc.paper.registry.RegistryAccess.registryAccess()
         .getRegistry(io.papermc.paper.registry.RegistryKey.ENCHANTMENT)
         .get(RESILIENCE_KEY);
-    private static final Material[] ARMOR_TYPES = {
-        Material.DIAMOND_HELMET, Material.DIAMOND_CHESTPLATE, Material.DIAMOND_LEGGINGS, Material.DIAMOND_BOOTS,
-        Material.NETHERITE_HELMET, Material.NETHERITE_CHESTPLATE, Material.NETHERITE_LEGGINGS, Material.NETHERITE_BOOTS
-    };
+
     private static final String[] LOOT_TABLES = {
         "minecraft:chests/ancient_city",
         "minecraft:chests/bastion_treasure",
@@ -30,10 +26,9 @@ public class ResilienceLoot extends AbstractLootHandler {
 
     @Override
     protected ItemStack createLootItem() {
-        Material armorType = ARMOR_TYPES[ThreadLocalRandom.current().nextInt(ARMOR_TYPES.length)];
-        ItemStack armor = new ItemStack(armorType);
-        armor.addUnsafeEnchantment(RESILIENCE_ENCHANT, 1);
-        return armor;
+        ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
+        book.addUnsafeEnchantment(RESILIENCE_ENCHANT, 1);
+        return book;
     }
 
     @Override
